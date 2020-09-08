1f0d79829898fa8c83c71811f52b836bc6333ca2
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-08 20:17:21 +0530

    Spring Boot basic app and web app using spring boot added

---
+++ 03WebAppWithSpringBoot/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		//1. Loads the defaults
+		//2. Starts the boot app
+		//3. Performs class path scan
+		//4. Starts local tomcat
+	}
+
+}
