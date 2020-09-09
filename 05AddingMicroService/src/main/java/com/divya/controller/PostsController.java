94f2d01f47448e4c092c26eb8e6c6a0a53a9bee9
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-09 13:20:21 +0530

    Get method with specific value added

--- 05AddingMicroService/src/main/java/com/divya/controller/PostsController.java
+++ 05AddingMicroService/src/main/java/com/divya/controller/PostsController.java
@@ -3,6 +3,7 @@ package com.divya.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class PostsController {
 	@RequestMapping("/posts")
 	public List<Post> getPosts() {
 		return new PostsService().getPosts();
+	}
+	
+	@RequestMapping("/posts/{id}")
+	public Post getPost(@PathVariable int id) {
+		return new PostsService().getPost(id);
 	}
 	
 }
