564ec23ff0b8d51269abc158d9fd92ece3587eb5
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-09 15:54:51 +0530

    POST, PUT and DELETE operations added

--- 05AddingMicroService/src/main/java/com/divya/controller/PostsController.java
+++ 05AddingMicroService/src/main/java/com/divya/controller/PostsController.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.divya.entity.Post;
@@ -23,4 +26,18 @@ public class PostsController {
 		return new PostsService().getPost(id);
 	}
 	
+	@RequestMapping(method = RequestMethod.POST, value = "/posts")
+	public void addPost(@RequestBody Post postElement) {
+		new PostsService().addPost(postElement);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/posts/{id}")
+	public void updatePost(@RequestBody Post post, @PathVariable int id) {
+		new PostsService().updatePost(post, id);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/posts/{id}")
+	public void deletePost(@PathVariable int id) {
+		new PostsService().deletePost(id);
+	}
 }
--- 05AddingMicroService/src/main/java/com/divya/services/PostsService.java
+++ 05AddingMicroService/src/main/java/com/divya/services/PostsService.java
@@ -1,6 +1,7 @@
 package com.divya.services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -10,15 +11,16 @@ import com.divya.entity.Post;
 @Service
 public class PostsService {
 	
-	List<Post> posts = new ArrayList<Post>();
-	
-	public PostsService() {
-		posts.add(new Post(1, "DataType", "Sample text"));
-		posts.add(new Post(2, "Classes", "Sample text"));
-		posts.add(new Post(3, "Objects", "Sample text"));
-		posts.add(new Post(4, "Specifiers", "Sample text"));
-		posts.add(new Post(5, "OOPS", "Sample text"));
-	}
+
+	static List<Post> posts = new ArrayList<Post>(
+			Arrays.asList(
+					new Post(1, "DataType", "Sample text"),
+					new Post(2, "Classes", "Sample text"),
+					new Post(3, "Objects", "Sample text"),
+					new Post(4, "Specifiers", "Sample text"),
+					new Post(5, "OOPS", "Sample text")
+					
+					));
 	
 	public List<Post> getPosts() {
 		return posts;
@@ -31,6 +33,32 @@ public class PostsService {
 			}
 		}
 		return null;
+	}
+
+	public void addPost(Post postElement) {
+		posts.add(postElement);
+	}
+
+	public void updatePost(Post post, int id) {
+		for(int i=0;i<posts.size();i++) {
+			Post tempPost = posts.get(i);
+			if(tempPost.getPostId() == id) {
+				posts.set(i, post);
+				return;
+			}
+		}
+		
+	}
+
+	public void deletePost(int id) {
+		for(int i=0;i<posts.size();i++) {
+			Post tempPost = posts.get(i);
+			if(tempPost.getPostId() == id) {
+				posts.remove(i);
+				return;
+			}
+		}
+		
 	}
 	
 }
