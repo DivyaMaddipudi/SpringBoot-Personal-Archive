8386c53f4cbcbe240db1336dad127451e4312e63
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-10 08:00:54 +0530

    Spring MVC setup added

---
+++ 07Store/src/main/java/ogr/divya/store/Store.java
@@ -0,0 +1,13 @@
+package ogr.divya.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Store {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
