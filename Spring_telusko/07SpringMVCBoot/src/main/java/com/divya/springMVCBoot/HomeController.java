371f6fb784f6e536f20456d9cf4864eb3e9a0930
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-16 19:42:59 +0530

    ModelAndView used

--- Spring_telusko/07SpringMVCBoot/src/main/java/com/divya/springMVCBoot/HomeController.java
+++ Spring_telusko/07SpringMVCBoot/src/main/java/com/divya/springMVCBoot/HomeController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -17,12 +18,14 @@ public class HomeController {
 	}
 	
 	@RequestMapping("add")
-	public String add(@RequestParam("num1") int firstNumber, @RequestParam("num2") int secondNumber, HttpSession session) {
+	public ModelAndView add(@RequestParam("num1") int firstNumber, @RequestParam("num2") int secondNumber) {
+		
+		ModelAndView modelAndView = new ModelAndView("result.jsp");
 		
 		int result = firstNumber + secondNumber;
 		
-		session.setAttribute("result", result);
+		modelAndView.addObject("result", result);
 		
-		return "result.jsp";
+		return modelAndView;
 	}
 }
