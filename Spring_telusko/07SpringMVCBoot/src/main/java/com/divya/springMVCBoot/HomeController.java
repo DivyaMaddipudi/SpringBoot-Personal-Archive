2277a6730a472343319736bb20054e64ff6cf363
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-16 19:23:01 +0530

    Accepting user input in spring boot

--- Spring_telusko/07SpringMVCBoot/src/main/java/com/divya/springMVCBoot/HomeController.java
+++ Spring_telusko/07SpringMVCBoot/src/main/java/com/divya/springMVCBoot/HomeController.java
@@ -1,5 +1,8 @@
 package com.divya.springMVCBoot;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +13,18 @@ public class HomeController {
 	public String home() {
 		
 		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public String add(HttpServletRequest req) {
+		
+		int firstNumber = Integer.parseInt(req.getParameter("num1"));
+		int secondNumber = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = firstNumber + secondNumber;
+		HttpSession session = req.getSession();
+		session.setAttribute("result", result);
+		
+		return "result.jsp";
 	}
 }
